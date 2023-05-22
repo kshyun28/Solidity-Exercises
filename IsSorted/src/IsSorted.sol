a2a3ff6bc4a98aa2fe63e8153f6642c21d3dfb23
kshyun28 <jasper.d.gabriel@gmail.com>
2023-05-22 21:25:38 +0800

    solve "arrays and strings" practice problems

--- IsSorted/src/IsSorted.sol
+++ IsSorted/src/IsSorted.sol
@@ -6,6 +6,11 @@ contract IsSorted {
      * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
      */
     function isSorted(uint256[] calldata arr) public view returns (bool) {
-        // your code here
+        for (uint256 i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
