0f8d63b90cc2971e33ca1658d4f76b2955a257ac
kshyun28 <jasper.d.gabriel@gmail.com>
2023-05-22 21:26:19 +0800

    solved "nested arrays" practice problems

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,23 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        // Horizontal
+        bool cond1 = board[0][0] == board[0][1] && board[0][0] == board[0][2];
+        bool cond2 = board[1][0] == board[1][1] && board[1][0] == board[1][2];
+        bool cond3 = board[2][0] == board[2][1] && board[2][0] == board[2][2];
+        // Vertical
+        bool cond4 = board[0][0] == board[1][0] && board[0][0] == board[2][0];
+        bool cond5 = board[0][1] == board[1][1] && board[0][1] == board[2][1];
+        bool cond6 = board[0][2] == board[1][2] && board[0][2] == board[2][2];
+        // Diagonal
+        bool cond7 = board[0][0] == board[1][1] && board[0][0] == board[2][2];
+        bool cond8 = board[0][2] == board[1][1] && board[0][2] == board[2][0];
+
+        if (
+            cond1 || cond2 || cond3 || cond4 || cond5 || cond6 || cond7 || cond8
+        ) {
+            return true;
+        }
+        return false;
     }
 }
